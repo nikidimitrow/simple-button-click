d46b76d20fd2a21f770547945fe40cafcb22fb72
nikidimitrow <nikidimitrow@gmail.com>
2017-01-06 12:01:30 +0200

    - gmail access

--- simple-button-click.py
+++ simple-button-click.py
@@ -1,9 +1,19 @@
-import subprocess
-import webbrowser
-import sys
+import urllib.request
 
-url = 'http://stackoverflow.com/'
-if sys.platform == 'darwin':    # in case of OS X
-    subprocess.Popen(['open', url])
-else:
-    webbrowser.open_new_tab(url)
+def get_unread_msgs(user, passwd):
+    auth_handler = urllib.request.HTTPBasicAuthHandler()
+    auth_handler.add_password(
+        realm='New mail feed',
+        uri='https://mail.google.com',
+        user='%s@gmail.com' % user,
+        passwd=passwd
+    )
+    opener = urllib.request.build_opener(auth_handler)
+    urllib.request.install_opener(opener)
+    feed = urllib.request.urlopen('https://mail.google.com/mail/feed/atom')
+    return feed.read()
+
+print (get_unread_msgs("",""))
+
+
+
