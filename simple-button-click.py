43914044909b63809849f9d6d2a231dbf9100df0
nikidimitrow <nikidimitrow@gmail.com>
2017-01-06 16:50:10 +0200

    - autologin form is completed

--- simple-button-click.py
+++ simple-button-click.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 usernameStr = '#'
 passwordStr = '#'
 
@@ -19,7 +20,7 @@ password = WebDriverWait(browser, 10).until(
     EC.presence_of_element_located((By.ID, 'user_pass')))
 password.send_keys(passwordStr)
 
-signInButton = browser.find_element_by_id('user_login')
+signInButton = browser.find_element_by_id('wp-submit')
 signInButton.click()
 
 
