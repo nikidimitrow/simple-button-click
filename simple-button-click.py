2a5c5cbaeae96ae2d53b5550663d797f9a42c5d3
nikidimitrow <nikidimitrow@gmail.com>
2017-01-06 16:46:59 +0200

    - bug fix

--- simple-button-click.py
+++ simple-button-click.py
@@ -3,22 +3,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-usernameStr = 'YOUR_USERNAME'
-passwordStr = 'YOUR_PASS'
+usernameStr = '#'
+passwordStr = '#'
 
 browser = webdriver.Chrome()
-browser.get(('http://YOUR_WEB_SITE/login.php'))
+browser.get(('#'))
 
 username = browser.find_element_by_id('user_login')
 username.send_keys(usernameStr)
+
 nextButton = browser.find_element_by_id('user_pass')
 nextButton.click()
 
 password = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.ID, 'user_login')))
+    EC.presence_of_element_located((By.ID, 'user_pass')))
 password.send_keys(passwordStr)
 
-signInButton = browser.find_element_by_id('user_pass')
+signInButton = browser.find_element_by_id('user_login')
 signInButton.click()
 
 
